21a11b9b8c4dc19ad521dde1fa2a4840a85d4193
shrav7130 <137476060+shrav7130@users.noreply.github.com>
2023-06-27 18:31:44 +0530

    Add files via upload

---
+++ Assignment_2.py
@@ -0,0 +1,73 @@
+# 1) Arithmetic operators
+x=20
+y=10
+print("addition of two nums: ",x+y)
+print("suytraction of two nums: ",x-y)
+print("multiplication of two nums: ",x*y)
+print("division of two nums: ",x/y)
+print("floor division of two nums: ",x//y)
+print("exponent : ",x**y)
+print("mod: ",x%y)
+
+# 2) Assignment operators
+x=10
+x+=1
+print("+=",x)
+x=10
+x-=1
+print("-=",x)
+x=10
+x*=1
+print("*=",x)
+x=10
+x/=1
+print("/=",x)
+x=10
+x**=1
+print("**=",x)
+x=10
+x//=1
+print("//=",x)
+x=10
+x%=1
+print("%=",x)
+
+
+# 3)Comparision operators / Relational operators
+x=20
+y=4
+print(">=",x>=y)
+print("<=",x<=y)
+print(">",x>y)
+print("<",x<y)
+print("==",x==y)
+print("!=",x!=y)
+
+# 4) Logical operators
+x=True
+y=False
+print("and",x and y)
+print("or",x or y)
+print("not",not y)
+
+# 5)Bitwise operators
+x=10
+y=5
+print("bitwise &",x&y)
+print("bitwise |",x|y)
+print("bitwise ^",x^y)
+print("bitwise >>",x>>y)
+print("bitwise <<",x<<y)
+
+
+# 6)Identity operators
+x=10
+y=5
+print("is",x is y)
+print("is not",x is not y)
+
+#7)Membership operators
+x="Shravan"
+print('S' in x)
+print("d" not in x)
+
