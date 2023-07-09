8a0c3d98a20d347ef7f40e8f5ee7fd4bb201e7ee
shrav7130 <137476060+shrav7130@users.noreply.github.com>
2023-07-09 23:59:09 +0530

    Add files via upload

---
+++ issue_book.py
@@ -0,0 +1,11 @@
+
+
+def issue_book():
+    book_title = input("Enter the title of the book: ")
+    member_id = input("Enter the ID of the member: ")
+    f=open("issue.txt","a")
+    f.write(f"{book_title},{member_id}")
+    print("Book issued successfully!")
+
+
+    
